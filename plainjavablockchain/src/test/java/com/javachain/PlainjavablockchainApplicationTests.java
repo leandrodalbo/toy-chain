c3b1a6f77f57c2003b6e89f70079c0db1482b6fc
leandro dal bo <leandrod@flutterint.com>
2022-11-04 14:15:09 +0000

    starting a blockchaing in plain java

---
+++ plainjavablockchain/src/test/java/com/javachain/PlainjavablockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javachain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlainjavablockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
